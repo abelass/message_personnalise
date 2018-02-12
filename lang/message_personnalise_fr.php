f396581d3ed8cff70e66b0b25113e6b0abf6135b
abelass <rainer@websimple.be>
2018-02-12 15:57:07 +0100

    attacher les messages aux objets choisis

--- lang/message_personnalise_fr.php
+++ lang/message_personnalise_fr.php
@@ -2,19 +2,18 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 if (!defined('_ECRIRE_INC_VERSION')) {
-	return;
+  return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// M
-	'message_personnalise_titre' => 'Message personnalisé',
+  // M
+  'message_personnalise_titre' => 'Message personnalisé',
 
-	// C
-	'cfg_exemple' => 'Exemple',
-	'cfg_exemple_explication' => 'Explication de cet exemple',
-	'cfg_titre_parametrages' => 'Paramétrages',
+  // C
+  'cfg_titre_parametrages' => 'Paramétrages',
+  'configurer_objets_label' => 'Activer l’ajout de messages personnalisés sur les contenus :',
 
-	// T
-	'titre_page_configurer_message_personnalise' => 'Paramètres message personnalisé',
+  // T
+  'titre_page_configurer_message_personnalise' => 'Paramètres message personnalisé',
 );
