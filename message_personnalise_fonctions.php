140e210af3003059cd3ed2f1d5f934a07175ddff
abelass <rainer@websimple.be>
2018-03-06 10:32:47 +0100

    surcharge via array_merge

--- message_personnalise_fonctions.php
+++ message_personnalise_fonctions.php
@@ -25,20 +25,12 @@ function balise_MESSAGE_PERSONNALISE_dist($p) {
 	$message = interprete_argument_balise(2, $p);
 	$objets_cibles = interprete_argument_balise(3, $p);
 	$declencheurs = interprete_argument_balise(4, $p);
-	$args = interprete_argument_balise(5, $p);
-	spip_log($args, 'teste');
-	if (isset($args['objet']) && $args['id_objet']) {
-		$objet = $args['objet'];
-		$id_objet = $args['id_objet'];
-	}
-	else {
-		$_id_objet = $p->boucles[$p->id_boucle]->primary;
-		$id_objet = champ_sql($_id_objet, $p);
-		$objet = $p->boucles[$p->id_boucle]->id_table;
-	}
+	$options = interprete_argument_balise(5, $p);
+	$_id_objet = $p->boucles[$p->id_boucle]->primary;
+	$id_objet = champ_sql($_id_objet, $p);
+	$objet = $p->boucles[$p->id_boucle]->id_table;
 
-
-	$p->code = "calculer_balise_MESSAGE_PERSONNALISE('$objet', $id_objet, $nom, $message, $objets_cibles, $declencheurs, $args)";
+	$p->code = "calculer_balise_MESSAGE_PERSONNALISE('$objet', $id_objet, $nom, $message, $objets_cibles, $declencheurs, $options)";
 
 	return $p;
 }
@@ -57,18 +49,20 @@ function balise_MESSAGE_PERSONNALISE_dist($p) {
  * @param array $objets_cibles
  *        	Les objets auxquelles sont liés des messages.
  * @param array $declencheurs
- * @param array $args
+ * @param array $options
  *
  * @return string
  */
-function calculer_balise_MESSAGE_PERSONNALISE($objet, $id_objet, $nom, $message, $objets_cibles, $declencheurs, $args) {
+function calculer_balise_MESSAGE_PERSONNALISE($objet, $id_objet, $nom, $message, $objets_cibles, $declencheurs, $options) {
 	include_spip('inc/message_personnalise');
-	return chercher_message_personnalise($message, $nom, array(
-			'objet' => $objet,
-			'id_objet' => $id_objet,
-			'objets_cibles' => $objets_cibles,
-			'declencheurs' => $declencheurs,
-			'args' => $args,
+	return chercher_message_personnalise($message, $nom, array_merge(
+			array(
+				'objet' => $objet,
+				'id_objet' => $id_objet,
+				'objets_cibles' => $objets_cibles,
+				'declencheurs' => $declencheurs,
+			),
+			$options
 		)
 	);
 }
