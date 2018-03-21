56a39d13017ad49c9aa984c0d988b21812445866
abelass <rainer@websimple.be>
2018-03-21 14:56:34 +0100

    traduction en allemand

---
+++ lang/message_personnalise_de.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'message_personnalise_titre' => 'Personalisierte Nachricht',
+
+	// C
+	'cfg_titre_parametrages' => 'Einstellungen',
+	'configurer_objets_label' => 'Activer l’ajout de messages personnalisés sur les contenus :',
+
+	// T
+	'titre_page_configurer_message_personnalise' => 'Einstellungen Personalisierte Nachricht',
+);
---
+++ lang/mp_message_de.php
@@ -0,0 +1,52 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_mp_message' => 'Ajouter ce message',
+
+	// C
+	'champ_declencheur_qui_label' => 'Auslöser "wer"',
+	'champ_declencheur_quoi_label' => 'Auslöser "was"',
+	'champ_declencheur_statut_label' => 'Auslöser "Status"',
+	'champ_descriptif_explication' => 'Interner Gebrauch',
+	'champ_descriptif_label' => 'Beschreibung',
+	'champ_texte_explication' => 'Personalisierte Nachricht.',
+	'champ_texte_explication_inclures' => 'Sie können die folgenden Einsätze @inclures@ benützen',
+	'champ_texte_explication_raccoursis' => 'Sie können die folgenden Abkürzungen @champs@ benützen',
+	'champ_type_label' => 'Typ',
+	'confirmer_supprimer_mp_message' => 'Bestätigen Sie das Löschen dieser Nachricht?',
+
+	// I
+	'icone_creer_mp_message' => 'Eine personalisierte Nachricht erstellen',
+	'icone_modifier_mp_message' => 'Eine personalisierte Nachricht bearbeiten',
+	'info_1_mp_message' => 'Eine personalisierte Nachricht',
+	'info_aucun_mp_message' => 'Keine personalisierte Nachricht',
+	'info_mp_messages_auteur' => 'Die personalisierten Nachrichten dieses Autoren',
+	'info_nb_mp_messages' => '@nb@ personalisierte Nachrichten',
+
+	// R
+	'retirer_lien_mp_message' => 'Diese personalisierte Nachricht entfernen',
+	'retirer_tous_liens_mp_messages' => 'Alle personalisierte Nachricht entfernen',
+
+	// S
+	'supprimer_mp_message' => 'Diese personalisierte Nachricht löschen',
+
+	// T
+	'texte_ajouter_mp_message' => 'Eine personalisierte Nachricht hinzufügen',
+	'texte_changer_statut_mp_message' => 'Diese personalisierte Nachricht ist :',
+	'texte_creer_associer_mp_message' => 'Erstellen und verknüpfen einer personalisierte Nachricht',
+	'texte_definir_comme_traduction_mp_message' => 'Diese personalisierte Nachricht ist eine Übersetzung der personalisierten Nachrichte mit Nummer::',
+	'titre_langue_mp_message' => 'Sprache dieser personalisierten Nachricht',
+	'titre_logo_mp_message' => 'Logo dieser personalisierten Nachricht',
+	'titre_mp_message' => 'Personalisierte Nachricht',
+	'titre_mp_messages' => 'Personalisierte Nachrichten',
+	'titre_mp_messages_rubrique' => 'Personalisierte Nachricht der Rubrik',
+	'titre_objets_lies_mp_message' => 'An diese Personalisierte Nachricht verknüpft',
+);
